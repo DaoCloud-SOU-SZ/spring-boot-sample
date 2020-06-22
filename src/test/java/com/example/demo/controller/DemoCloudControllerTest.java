0144a9f130c1b1420afd2d83912310db9a316714
qilong.qiu <qilong.qiu@daocloud.io>
2020-06-22 16:16:31 +0800

    添加单元测试

--- src/test/java/com/example/demo/controller/DemoCloudControllerTest.java
+++ src/test/java/com/example/demo/controller/DemoCloudControllerTest.java
@@ -1,29 +1,46 @@
 package com.example.demo.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {SpringBootDemoApplication.class})
-@AutoConfigureMockMvc
+@SpringBootTest
+//@AutoConfigureMockMvc
 public class DemoCloudControllerTest {
-	@Autowired
-	private MockMvc mvc;
+	//@Autowired
+	private MockMvc mockMvc;
 
-	@Test
-	public void versionTest() throws Exception {
-		this.mvc.perform(get("/cloud/value")).andExpect(status().isOk())
-				.andExpect(content().string("{\"version\":\"1.0.0\",\"message\":\"hello world\"}"))
-                .andDo(MockMvcResultHandlers.print());
-	}
+    @Autowired
+    private DemoCloudController demoCloudController;
+    
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("init demoCloudController");
+        mockMvc = MockMvcBuilders.standaloneSetup(demoCloudController).build();
+    }
+    
+    @Test
+    public void demoCloudControllerTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/cloud/value");
+        ResultActions result = mockMvc.perform(request);
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("{\"version\":\"1.0.0\",\"message\":\"hello cloud\"}")) 
+                .andDo(MockMvcResultHandlers.print()) 
+                .andReturn(); 
+    }
+
 }
--- src/test/java/com/example/demo/controller/DemoControllerTest.java
+++ src/test/java/com/example/demo/controller/DemoControllerTest.java
@@ -1,28 +1,43 @@
 package com.example.demo.controller;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @RunWith(SpringRunner.class)
-@SpringBootTest(classes = {SpringBootDemoApplication.class})
-@AutoConfigureMockMvc
+@SpringBootTest
 public class DemoControllerTest {
-	@Autowired
-	private MockMvc mvc;
+	private MockMvc mockMvc;
 
-	@Test
-	public void versionTest() throws Exception {
-		this.mvc.perform(get("/demo/version")).andExpect(status().isOk())
-				.andExpect(content().string("{\"version\":\"1.0.0\",\"message\":\"hello world\"}"))
-                .andDo(MockMvcResultHandlers.print());
-	}
+    @Autowired
+    private DemoController demoController;
+    
+    @Before
+    public void setUp() throws Exception {
+        System.out.println("init demoController");
+        mockMvc = MockMvcBuilders.standaloneSetup(demoController).build();
+    }
+    
+    @Test
+    public void demoControllerTest() throws Exception {
+        RequestBuilder request = MockMvcRequestBuilders.get("/demo/version");
+        ResultActions result = mockMvc.perform(request);
+        result.andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("{\"version\":\"1.0.0\",\"message\":\"hello world\"}")) 
+                .andDo(MockMvcResultHandlers.print()) 
+                .andReturn(); 
+    }
 }
