ddf13b77a353c25e8753d6f7d47818bb9de9f2a9
Administrator <sou-notifications@daocloud.io>
2022-03-22 16:30:22 +0000

    Add new file

---
+++ src/main/java/com/example/demo/utils/LoadProperties.java
@@ -0,0 +1,17 @@
+com.example.demo.utils
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+ 
+@Component
+@ConfigurationProperties(prefix = "prometheus-histogram-metrics")
+public class LoadProperties {
+    public String getRegex() {
+        return regex;
+    }
+ 
+    public void setRegex(String regex) {
+        this.regex = regex;
+    }
+ 
+    private String regex;
+}
