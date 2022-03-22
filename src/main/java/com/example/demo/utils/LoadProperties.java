b967d53850e0dce2ac5ed6744f7dae9972212bc4
Administrator <sou-notifications@daocloud.io>
2022-03-22 16:32:39 +0000

    Update LoadProperties.java

--- src/main/java/com/example/demo/utils/LoadProperties.java
+++ src/main/java/com/example/demo/utils/LoadProperties.java
@@ -1,4 +1,4 @@
-package com.example.demo.utils
+package com.example.demo.utils;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
  
